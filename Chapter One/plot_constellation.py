b2b43a82bc6d542e45a36b0d07e9beb3f57fee6d
quartzalotl <lc03711@gmail.com>
2025-04-29 21:29:16 -0700

    finished constellation

--- Chapter One/plot_constellation.py
+++ Chapter One/plot_constellation.py
@@ -11,7 +11,7 @@ screen = pygame.display.set_mode((screen_width, screen_height)) # get_mode: crea
 def draw_star(x, y, brightness):
     pygame.draw.rect(screen, white, (x, y, brightness, brightness))
 
-gemini_half = [
+gemini = [
     (100, 50, 15),
     (130, 160, 10),
     (45, 189, 11),
@@ -19,7 +19,16 @@ gemini_half = [
     (160, 350, 11),
     (75, 450, 12),
     (135, 560, 14),
-    (300, 545, 13),
+    (280, 545, 13),
+
+    (350, 160, 10),
+    (310, 45, 14),
+    (490, 130, 13),
+    (395, 390, 10),
+    (370, 510, 13),
+    (450, 500, 9),
+    (480, 505, 10),
+    (507, 505, 9)
 ]
 
 # Keep graphics window open
@@ -28,7 +37,7 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # pygame.QUIT is an event triggered when there is an attempt tp close the window
             done = True
-    for star in gemini_half:
+    for star in gemini:
         draw_star(*star)
     pygame.display.update()
 pygame.quit()
